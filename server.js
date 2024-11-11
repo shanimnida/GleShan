a6949429ac96239afa678a0157e61f3a10bbcd84
Shan Malong <shanimnida69@gmail.com>
2024-11-11 17:53:19 +0800

    Update Server

--- server.js
+++ server.js
@@ -271,8 +271,6 @@ async function connectDB() {
     const uri = process.env.MONGODB_URI;
     
     const client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       retryWrites: true,
       w: 'majority',
     });
