1080f23601962a7f1df3b485e2dea4ca712eddbc
shanimnida <119436260+shanimnida@users.noreply.github.com>
2024-11-11 16:46:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static('public'));
 
 
 // MongoDB URI for session management
-const mongoURI = process.env.MONGO_URI + "?ssl=true";
+const mongoURI = process.env.MONGO_URI;
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
